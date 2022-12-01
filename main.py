4fd2deb90b6876b38286eacd1d501376baacb21d
5v3nn <sven.greuter@protonmail.ch>
2022-12-01 22:19:07 +0100

    upload version 0.1.0.4

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from save_system import SaveSystem
 KIVY_FILE_SCREEN_MANAGER = "./templates/screen_manager.kv"
 
 # set version
-__version__ = "0.1.0.3"  # change in the buildozer.spec too
+__version__ = "0.1.0.4"  # change in the buildozer.spec too
 
 # Set screen size (didn't work)
 # Config.set('graphics', 'resizable', 1)
